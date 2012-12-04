95db3a3150e721295a690ee238e3fa127871c48a
aardvarkk <clarksonian@gmail.com>
2012-12-03 22:49:43 -0500

    Added nifty authentication.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newtube::Application.config.secret_token = '0f49b22c6f779cd1566ade7614cdb97eb7f23b84647a69bfbb62e45edee56a703be4293520b671b295c6581f9c4bbc575aa9bb6d0f784b551153b9832c1400d1'
