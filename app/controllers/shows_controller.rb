84b7fc425122fb605caa126f18ddb5e22c0f64c2
aardvarkk <clarksonian@gmail.com>
2012-12-04 00:47:38 -0500

    Just showing some basic views of what things might look like. Added the ability to create a show with the form.

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,4 +1,14 @@
 class ShowsController < ApplicationController
+  before_filter :login_required
+
   def index
+    # Set up some test data
+    @shows = ['Elementary', 'Hart of Dixie']
   end
+
+  def create
+    # Try to find the show in the database
+    redirect_to user_shows_path(current_user), notice: 'Hi!'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Newtube::Application.routes.draw do
   
-  get "shows/index"
-
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'users#new'
